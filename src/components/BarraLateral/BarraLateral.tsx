c1f249f6338666f88df17955c355c98f416be9b2
thainarapenha <thainarapenha@alu.ufc.br>
2023-03-06 11:20:41 -0300

    feat: adicionando menu de projetos

--- src/components/BarraLateral/BarraLateral.tsx
+++ src/components/BarraLateral/BarraLateral.tsx
@@ -1,59 +1,44 @@
 import "./style.scss";
-import React, { useState } from 'react';
+import React from 'react';
+import List from "@mui/material/List/List";
 import { RootState } from "../../store";
 import { useDispatch, useSelector } from "react-redux";
-import AddIcon from '@mui/icons-material/Add';
-import LabelImportantIcon from '@mui/icons-material/LabelImportant';
-import { addProjeto } from "../../store/projectSlicer";
-import { ToastContainer, toast } from 'react-toastify';
+import { ItemProject } from "../ItemProject/ItemProject";
+import { changeProject } from "../../store/projectSlicer";
+import { FormsAddProject } from "../FormsAddProject/FormsAddProject";
 
 export const BarraLateral: React.FC = () => {
-
-  const [novoProjeto, setNovoProjeto] = useState("");
-  const project = useSelector((state: RootState) => state.project.projetos);
-  const dispach = useDispatch();
-
-
-  const AddNovoProjeto = () => {
-    if (novoProjeto == "") {
-      toast.error("Digite um nome para o projeto");
-      return;
-    } else {
-      dispach(addProjeto(novoProjeto));
-      setNovoProjeto("");
-      toast.success("Projeto criado com sucesso!");
-    }
-  }
+  const [inbox, today, week, ...projects] = useSelector(
+    (state: RootState) => state.project.projetos,
+  );
+  const dispatch = useDispatch();
+  
+  const ClickProject = (id: number) => {
+    dispatch(changeProject(id));
+  };
 
   return (
     <div className="menu">
-      <nav>
-        <ul>
-          <h2>Menu</h2>
-
-          {project.map((item, key) => (
-            <li key={key}>
-              <LabelImportantIcon /> | {item.title}
-            </li>
-          ))}
-
-          <ToastContainer
-            autoClose={2500}
-            pauseOnHover={false}
+      <h2>Menu</h2>
+      <List>
+      {projects.map(project => (
+          <ItemProject
+            id={project.id}
+            key={project.id}
+            title={project.title}
+            onClick={() => ClickProject(project.id)}
           />
-          <div className="btn" >
-            <input
-              type="text"
-              placeholder=" Adicionar projeto"
-              value={novoProjeto}
-              onChange={(event) => setNovoProjeto(event.target.value)}
-            />
-            <AddIcon style={{cursor: "pointer", background: "white", padding: "4px"}}
-              onClick={AddNovoProjeto}
-            />
-          </div>
-        </ul>
-      </nav>
+        ))}
+
+        <FormsAddProject/>
+        
+        <ItemProject
+          id={inbox.id}
+          key={inbox.id}
+          title={inbox.title} 
+          onClick={() => ClickProject(inbox.id)}
+        />
+      </List>
     </div>
   );
 }
